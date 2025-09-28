6015447a0a5fd280f8646a11b151e6c309d5bf39
VipulSingh-10 <vsvipul10@gmail.com>
2025-09-28 06:21:30 +0200

    changes to CORS

--- vehicle-service/src/main/java/com/singhv/vehicleservice/controller/VehicleController.java
+++ vehicle-service/src/main/java/com/singhv/vehicleservice/controller/VehicleController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
-@CrossOrigin
 @RestController
 @RequestMapping("/api/vehicles")
 public class VehicleController {
